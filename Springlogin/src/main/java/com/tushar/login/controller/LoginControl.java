7bd85a73a71b5f4def0bd9dcc1ecc78ed53152eb
Tushar Kumar <tbehera188@gmail.com>
2025-11-21 11:28:37 +0530

    new adding

---
+++ Springlogin/src/main/java/com/tushar/login/controller/LoginControl.java
@@ -0,0 +1,17 @@
+package com.tushar.login.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginControl {
+     
+	@RequestMapping("/login")
+	public ModelAndView login() {
+		System.out.println("LoginController.login()::::::::::::::::::::::::::::::::::::::::::::::::::::::;");
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("login");
+		return mv;
+	}
+}
